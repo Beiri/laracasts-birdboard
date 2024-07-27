f57071ebe72a515d28f4401f2e9619dcfacfba07
Beiri Alife <beiri.al@outlook.com>
2024-07-27 17:17:07 -0300

    Ep 02 - Let's Begin With a Test

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
+use App\Project;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', 'ProjectsController@index');
+
+Route::post('/projects', 'ProjectsController@store');
