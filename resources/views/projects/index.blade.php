c1a594ae1eb8d83e2488d94fefc3b96f7f2b145b
Beiri Alife <beiri.al@outlook.com>
2024-07-28 16:15:27 -0300

    Ep 08 - Prepping the Frontend

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Birdboard</title>
-</head>
-
-<body>
-    <h1>Birdboard</h1>
+@section('content')
+    <div class="flex items-center mb-3">
+        <a href="/projects/create">New Project</a>
+    </div>
 
     <ul>
         @forelse ($projects as $project)
@@ -20,6 +14,4 @@
             <li>No project yet.</li>
         @endforelse
     </ul>
-</body>
-
-</html>
+@endsection
